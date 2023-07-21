e395309d7c70b220421a29f0bf9bec4c3ed6bbd1
Alex Mangust <sanya.stogny@gmail.com>
2023-07-22 03:52:21 +0800

    Домашнее задание, 1 семинар - выполнили 3 задачу

--- HomeworkSeminar1/Program.cs
+++ HomeworkSeminar1/Program.cs
@@ -26,24 +26,24 @@ a = -9, b = -3 -> max = -3*/
 44, 5, 72 -> 78
 22, 3, 9 -> 22*/
 
-Console.Write("Введите первое число: ");
-int OneNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int TwoNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите третье число: ");
-int ThreeNumber = Convert.ToInt32(Console.ReadLine());
-if((OneNumber > TwoNumber) && (OneNumber > ThreeNumber))
-{
-    Console.WriteLine(OneNumber);
-}
-else if((TwoNumber > OneNumber) && (TwoNumber > ThreeNumber))
-{
-    Console.WriteLine(TwoNumber);
-}
-else
-{
-    Console.WriteLine(ThreeNumber);
-}
+// Console.Write("Введите первое число: ");
+// int OneNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int TwoNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int ThreeNumber = Convert.ToInt32(Console.ReadLine());
+// if((OneNumber > TwoNumber) && (OneNumber > ThreeNumber))
+// {
+//     Console.WriteLine(OneNumber);
+// }
+// else if((TwoNumber > OneNumber) && (TwoNumber > ThreeNumber))
+// {
+//     Console.WriteLine(TwoNumber);
+// }
+// else
+// {
+//     Console.WriteLine(ThreeNumber);
+// }
 
 
 /*6. Напишите программу, которая на ввод принимает число и выдает, является ли число четным
@@ -52,6 +52,16 @@ else
 -3 -> нет
 7 -> нет*/
 
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число чётное.");
+}
+else
+{
+    Console.WriteLine("Число нечётное.");
+}
 /*8. Напишите программу, которая на ввод принимает число
 (N), а на выводе показывает все чётные число от 1 до N.
 5 -> 2, 4
