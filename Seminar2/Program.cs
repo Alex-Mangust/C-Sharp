933c1ead75334d7b59627d6b1821de2795482cdc
Alex Mangust <sanya.stogny@gmail.com>
2023-07-24 18:52:48 +0800

    Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,12 +1,15 @@
-﻿// Написать программу, которая будет принимать на вход два числа и выводить, является ли
-// второе число кратным первому. Если число 2 не кратно 1, про программа выводит остаток
-// от деления.
+﻿// Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
 
 Console.WriteLine("Введите число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-if(num%num1 == 0) Console.WriteLine("Числа кратные");
-else Console.WriteLine("Остаток равен " + num%num1);
+if((num%7 == 0) && (num%23 == 0)) Console.WriteLine("Число кратное и 7, и 23");
+else if ((num%7 == 0) && (num%23 != 0)) Console.WriteLine("Число кратное только 7");
+else if ((num%7 != 0) && (num%23 == 0)) Console.WriteLine("Число кратное только 23");
+else Console.WriteLine("Число не кратное ни 7, ни 23");
