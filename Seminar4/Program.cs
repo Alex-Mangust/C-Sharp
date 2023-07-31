fe186e35a96b5773636eb811a6b55b8fc8c15aa9
Alex Mangust <sanya.stogny@gmail.com>
2023-07-31 18:16:12 +0800

    Начали работу с 4 семинаром

---
+++ Seminar4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 24.  Задача 24: Напишите программу, которая принимает на вход число 
+//(А) и выдаёт сумму чисел от 1 до А.
+
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+// int GetSumNum(int a)
+// {
+//     int sum = 0;
+//     for(int i = 0; i <= a; i++)
+//     {
+//         sum += i;
+//     }
+
+//     return sum;
+// }
+
+// Console.Write("Введите число: ");
+// int sum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(GetSumNum(sum));
