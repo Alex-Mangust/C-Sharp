e24ad024e87bef55ce193d28c69f10dcbf1f7aea
Alex Mangust <sanya.stogny@gmail.com>
2023-08-01 16:21:56 +0800

    Домашнее задание, 4 семинар - выполнили 3 задачу (отредактировал код)

--- HomeworkSeminar4/Program.cs
+++ HomeworkSeminar4/Program.cs
@@ -59,7 +59,7 @@
 int[] array = new int[8];
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(101);
+    array[i] = new Random().Next(0, 100);
     Console.Write(array[i]);
     if(i != array.Length-1)
     {
