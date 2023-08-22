89d4a50ece22eae80c003e57ba9c2bcd1d532bc4
Alex Mangust <sanya.stogny@gmail.com>
2023-08-22 18:47:20 +0800

    Семинар 6. Написали программу, которая переводит число, полученное от пользователя, в двоичное значение и выводит результат на экран

--- Seminar6/Program.cs
+++ Seminar6/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Задача 42. Напишите программу, которая будет преобразовывать десятичное число
+    в двоичное.
+    45 -> 101101
+    3  -> 11
+    2  -> 10
+*/
+
+
+// // Мой вариант решения: 
+
+// System.Console.Write("Введите число: ");
+// int number = Convert.ToInt32(System.Console.ReadLine());
+// string numberTwo = "";
+// while(number > 0)
+// {
+//     numberTwo = number%2 + numberTwo;
+//     number = number/2;
+// }
+// if(number == 0)
+// {
+//     numberTwo = Convert.ToString(number);
+// }
+// else if (number < 0)
+// {
+//     System.Console.WriteLine("Вы ввели отрицательное число!");
+// }
+// System.Console.WriteLine(numberTwo);
+
+// Мой вариант решения с помощью массива:
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int numTwo = num;
+int m = 0;
+while(numTwo > 0)
+{
+    numTwo /= 2;
+    m++;
+}
+int[] array = new int[m];
+for(int i = 1; i <= m; i++)
+{
+    array[m - i] = num%2;
+    num /= 2;
+}
+for(int j = 0; j < m; j++)
+{
+    System.Console.Write(array[j]);
+}
