781e349e5773a49fa33217b7d85802dd56d0f269
Alex Mangust <sanya.stogny@gmail.com>
2023-08-25 17:26:08 +0800

    Семинар 7. Написали программу, которая создает массив, заполненный случайными числами, после чего находит элементы, у которых оба индекса четные, и заменяет эти элементы на их квадраты

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -46,15 +46,70 @@
     2 3 4 5
 */
 
-int m = 3, n = 4;
+// int m = 3, n = 4;
+// int[,] arrayOne = new int[m, n];
+// void InputArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i,j] = i + j;
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(j == 0) System.Console.Write("|");
+//             System.Console.Write(array[i, j]);
+//             if(j == array.GetLength(1) - 1) System.Console.Write("|");
+//             else System.Console.Write("; ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+// InputArray(arrayOne);
+// PrintArray(arrayOne);
+
+/*  Задача 49. Задайте двумерный массив. Найдите элементы, у которых оба индекса
+    чётные, и замените эти элементы на их квадраты.
+    Например, изначально массив               Новый массив будет выглядеть
+    выглядит так:                             вот так:
+    1 4 7 2                                   1 4 7 2
+    4 9 2 3                                   4 81 2 9
+    8 4 2 4                                   8 4 2 4
+*/
+
+using System.Text.RegularExpressions;
+
+System.Console.Write("Введите первую размерность массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите вторую размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
 int[,] arrayOne = new int[m, n];
+
 void InputArray(int[,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {
-            array[i,j] = i + j;
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PowArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i%2 == 0 && j%2 == 0) array[i, j] = (int)Math.Pow(array[i, j], 2);
         }
     }
 }
@@ -73,4 +128,7 @@ void PrintArray(int[,] array)
     }
 }
 InputArray(arrayOne);
+PrintArray(arrayOne);
+System.Console.WriteLine();
+PowArray(arrayOne);
 PrintArray(arrayOne);
