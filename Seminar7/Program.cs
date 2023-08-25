d2de13d4eefdf3f0df63c7f9dd57bf2955b00152
Alex Mangust <sanya.stogny@gmail.com>
2023-08-26 00:18:24 +0800

    Семинар 7. Написали программу, которая создает массив, заполненный случайными числами, и находит сумму элементов, находящихся на главной диагонали

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -84,32 +84,77 @@
     8 4 2 4                                   8 4 2 4
 */
 
-using System.Text.RegularExpressions;
+// using System.Text.RegularExpressions;
+
+// System.Console.Write("Введите первую размерность массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Введите вторую размерность массива: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int[,] arrayOne = new int[m, n];
+
+// void InputArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PowArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(i%2 == 0 && j%2 == 0) array[i, j] = (int)Math.Pow(array[i, j], 2);
+//         }
+//     }
+// }
+// void PrintArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if(j == 0) System.Console.Write("|");
+//             System.Console.Write(array[i, j]);
+//             if(j == array.GetLength(1) - 1) System.Console.Write("|");
+//             else System.Console.Write("; ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+// InputArray(arrayOne);
+// PrintArray(arrayOne);
+// System.Console.WriteLine();
+// PowArray(arrayOne);
+// PrintArray(arrayOne);
+
+
+/*  Задача 51. Задайте двумерный массив. Найдите сумму элементов, находящихся на
+    главной диагонали (с индексами (0,0); (1,1) и т.д.)
+    Например, задан массив:
+    1 4 7 2
+    5 9 2 3
+    8 4 2 4
+    Сумма элементов главной диагонали 1+9+2 = 12
+*/
 
 System.Console.Write("Введите первую размерность массива: ");
 int m = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите вторую размерность массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int[,] arrayOne = new int[m, n];
-
 void InputArray(int[,] array)
 {
     for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-
-void PowArray(int[,] array)
-{
-    for(int i = 0; i < array.GetLength(0); i++)
-    {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {
-            if(i%2 == 0 && j%2 == 0) array[i, j] = (int)Math.Pow(array[i, j], 2);
+            array[i, j] = new Random().Next(10, 100);
         }
     }
 }
@@ -120,15 +165,27 @@ void PrintArray(int[,] array)
         for(int j = 0; j < array.GetLength(1); j++)
         {
             if(j == 0) System.Console.Write("|");
-            System.Console.Write(array[i, j]);
+            System.Console.Write(array[i,j]);
             if(j == array.GetLength(1) - 1) System.Console.Write("|");
             else System.Console.Write("; ");
         }
         System.Console.WriteLine();
     }
 }
+
+int SumArray(int[,] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(i == j) sum += array[i, j];
+        }
+    }
+    return sum;
+}
 InputArray(arrayOne);
 PrintArray(arrayOne);
 System.Console.WriteLine();
-PowArray(arrayOne);
-PrintArray(arrayOne);
+System.Console.WriteLine($"Сумма элементов главной диагонали - {SumArray(arrayOne)}");
