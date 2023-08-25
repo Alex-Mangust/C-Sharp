2ee733c558f75a7e7161aef8261290991d8b566e
Alex Mangust <sanya.stogny@gmail.com>
2023-08-26 02:24:55 +0800

    Домашнее задание, 7 семинар - выполнили 1 задачу

--- HomeworkSeminar7/Program.cs
+++ HomeworkSeminar7/Program.cs
@@ -7,6 +7,44 @@
     8 7,8 -7,1 9
 */
 
+
+System.Console.Write("Введите первую размерность массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите вторую размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] arrayOne = new double[m, n];
+int count = 0;
+void InputArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            int num = new Random().Next(-100, 100);
+            array[i, j] = new Random().NextDouble() * num;
+            if(array[i, j] > -10 && array[i, j] < 3) count++;
+        }
+    }
+}
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(j == 0) System.Console.Write("[");
+            System.Console.Write(Math.Round(array[i, j], 2));
+            if(j == array.GetLength(1) -1) System.Console.Write("]");
+            else System.Console.Write("; ");   
+        }
+        System.Console.WriteLine();
+    }
+}
+InputArray(arrayOne);
+PrintArray(arrayOne);
+System.Console.WriteLine($"В массиве {count} элементов находятся в диопазоне от -10 до 3.");
+
+
 /*  
     Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
