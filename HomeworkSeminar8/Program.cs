013f0a233ac9f8f2195cde96a7da0cbaa129f61e
Alex Mangust <sanya.stogny@gmail.com>
2023-08-30 03:17:32 +0800

    Домашнее задание, 8 семинар - выполнили 1 задачу

--- HomeworkSeminar8/Program.cs
+++ HomeworkSeminar8/Program.cs
@@ -1,5 +1,6 @@
 ﻿/*
-    Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+    Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки
+    двумерного массива.
  
     Например, задан массив:
     1 4 7 2
@@ -10,10 +11,74 @@
     9 5 3 2
     8 4 4 2
 */
+System.Console.Write("Введите первую размерность массива: ");
+int rows = Convert.ToInt32(System.Console.ReadLine());
+System.Console.Write("Введите первую размерность массива: ");
+int colums = Convert.ToInt32(System.Console.ReadLine());
+int[,] matrix = new int[rows, colums];
+void InputArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 100);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            if(j == 0)
+            {
+                System.Console.Write("[");
+            }
+            System.Console.Write(array[i,j]);
+            if(j == array.GetLength(1) - 1)
+            {
+                System.Console.Write("]");
+            }
+            else
+            {
+                System.Console.Write("; ");
+            }
+        }
+        System.Console.WriteLine();
+    }
+}
+void OrderingArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if(array[i, k] < array[i, k+1])
+                {
+                    int temp = 0;
+                    temp = array[i, k+1];
+                    array[i, k+1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+InputArray(matrix);
+PrintArray(matrix);
+System.Console.WriteLine();
+OrderingArray(matrix);
+PrintArray(matrix);
+
 
 
 /*
-    Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+    Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей
+    суммой элементов.
 
     Например, задан массив:
     1 4 7 2
@@ -37,7 +102,8 @@
 */
 
 /*  
-    Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+    Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет
+    построчно выводить массив, добавляя индексы каждого элемента.
     Массив размером 2 x 2 x 2
     66(0,0,0) 25(0,1,0)
     34(1,0,0) 41(1,1,0)
