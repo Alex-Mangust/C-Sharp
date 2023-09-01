abb3635da6462fec1d23f2c0f3c1ec08b3cb1297
Alex Mangust <sanya.stogny@gmail.com>
2023-09-01 21:40:38 +0800

    Семинар 9. Написали программу, которая выводит все натуральные числа в промежутке от 1 до N (с помощью рекурсии)

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Задача 63. Задайте значение N. Напишите программу, которая выведет все натуральные
+    числа в промежутке от 1 до N.
+*/
+
+// Мое решение:
+
+// int Recurcia(int N, int Min)
+// {
+//     if(N > Min)
+//     {
+//         System.Console.Write(Min);
+//         if(N > Min)
+//         {
+//             System.Console.Write("; ");
+//         }
+//         Min++;
+//         return Recurcia(N, Min);
+//     }
+//     else if(N < Min)
+//     {
+//         System.Console.Write(Min);
+//         if(N < Min)
+//         {
+//             System.Console.Write("; ");
+//         }
+//         Min--;
+//         return Recurcia(N, Min);
+//     }
+//     else
+//     {
+//         System.Console.WriteLine(N);
+//         return 0;
+//     }
+// }
+// System.Console.Write("Введите значение N: ");
+// int n = Convert.ToInt32(System.Console.ReadLine());
+// int min = 1;
+// Recurcia(n, min);
+
+
+// Решение преподователя:
+
+System.Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+string PrintNumbers(int start, int end)
+{
+    if(start == end)
+    {
+        return start.ToString();
+    }
+    if(end > start)
+    {
+        return (start + " " + PrintNumbers(start+1, end));
+    }
+    else
+    {
+        return (start + " " + PrintNumbers(start-1, end));
+    }
+}
+Console.WriteLine(PrintNumbers(1, num));
