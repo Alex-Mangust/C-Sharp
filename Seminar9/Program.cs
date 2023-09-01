5a3c8f1fc9f86d8e5b755b8f8cc377cce634cfa4
Alex Mangust <sanya.stogny@gmail.com>
2023-09-01 21:51:28 +0800

    Семинар 9. Написали программу, которая выводит все натуральные числа в промежутке от M до N (с помощью рекурсии)

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -41,8 +41,36 @@
 
 // Решение преподователя:
 
-System.Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
+// System.Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
+// string PrintNumbers(int start, int end)
+// {
+//     if(start == end)
+//     {
+//         return start.ToString();
+//     }
+//     if(end > start)
+//     {
+//         return (start + " " + PrintNumbers(start+1, end));
+//     }
+//     else
+//     {
+//         return (start + " " + PrintNumbers(start-1, end));
+//     }
+// }
+// Console.WriteLine(PrintNumbers(1, num));
+
+
+/*
+    Задача 65. Задайте значения M и N. Напишите программу, которая выведет все
+    натуральные числа в промежутке от M до N;
+*/
+
+System.Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine());
+System.Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+
 string PrintNumbers(int start, int end)
 {
     if(start == end)
@@ -51,11 +79,11 @@ string PrintNumbers(int start, int end)
     }
     if(end > start)
     {
-        return (start + " " + PrintNumbers(start+1, end));
+        return start + " " + PrintNumbers(start+1, end);
     }
     else
     {
-        return (start + " " + PrintNumbers(start-1, end));
+        return start + " " + PrintNumbers(start-1, end);
     }
 }
-Console.WriteLine(PrintNumbers(1, num));
+System.Console.Write("Числа в промежутке от M до N -> " + PrintNumbers(m, n));
