15d19670d40766559abf7b877694e9d69fc10851
Alex Mangust <sanya.stogny@gmail.com>
2023-09-01 22:17:33 +0800

    Семинар 9. Написали программу, которая принимает на вход число и возвращает сумму его цифр (с помощью рекурсии)

--- Seminar9/Program.cs
+++ Seminar9/Program.cs
@@ -66,24 +66,68 @@
     натуральные числа в промежутке от M до N;
 */
 
-System.Console.Write("Введите значение M: ");
-int m = int.Parse(Console.ReadLine());
-System.Console.Write("Введите значение N: ");
-int n = int.Parse(Console.ReadLine());
+// System.Console.Write("Введите значение M: ");
+// int m = int.Parse(Console.ReadLine());
+// System.Console.Write("Введите значение N: ");
+// int n = int.Parse(Console.ReadLine());
 
-string PrintNumbers(int start, int end)
+// string PrintNumbers(int start, int end)
+// {
+//     if(start == end)
+//     {
+//         return start.ToString();
+//     }
+//     if(end > start)
+//     {
+//         return start + " " + PrintNumbers(start+1, end);
+//     }
+//     else
+//     {
+//         return start + " " + PrintNumbers(start-1, end);
+//     }
+// }
+// System.Console.Write("Числа в промежутке от M до N -> " + PrintNumbers(m, n));
+
+
+/*
+    Задача 67. Напишите программу, которая будет принимать на вход число и возвращать
+    сумму его цифр.
+    453 -> 12    
+    45 -> 9
+*/
+
+
+// Мое решение
+
+// int Sum(string numbers, int sumNumbers, int count)
+// {
+//     if(count < numbers.Length)
+//     {
+//         sumNumbers += int.Parse(numbers[count].ToString());
+//         count++;
+//         return Sum(numbers, sumNumbers, count);
+//     }
+//     return sumNumbers;
+// }
+// System.Console.Write("Введите число: ");
+// string num = Console.ReadLine();
+// int sum = 0, count = 0;
+// System.Console.Write("Сумма чисел, введенного вами числа, равняется -> " + Sum(num, sum, count));
+
+
+// Решение преподователя
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(PrintNumbers(num));
+
+int PrintNumbers(int num)
 {
-    if(start == end)
-    {
-        return start.ToString();
-    }
-    if(end > start)
-    {
-        return start + " " + PrintNumbers(start+1, end);
-    }
-    else
-    {
-        return start + " " + PrintNumbers(start-1, end);
-    }
+if(num<=0)
+{
+return 0;
 }
-System.Console.Write("Числа в промежутке от M до N -> " + PrintNumbers(m, n));
+return (num%10+PrintNumbers(num/10));
+}
